1344fbd35b7365a25fb4f5c66c63b1a7be8686a4
Ezequiel <ezequielurrizaga@smartadtags.com>
2017-12-21 16:58:11 -0300

    z

--- proyecto 4/src/components/posts_new.js
+++ proyecto 4/src/components/posts_new.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    render () {
+    renderTitleField(field) {
         return (
             <div>
-                PostNew
+                <input
+                    type="text" 
+                /*
+                    el campo de abajo bindea el Field superior con el input en todo lo que hace a sus listeners, esto incluye eventos tales como:
+                    onChange={field.input.onChange}
+                    onFocus={field.input.onFocus}
+                    onBlur={field.input.onBlur}
+
+                 */
+                    {...field.input}
+                />
             </div>
+        );
+    }
+    render () {
+        return (
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField} />
+            </form>
         )
     }
 }
