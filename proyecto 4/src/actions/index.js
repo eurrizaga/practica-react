b2ae5aeaa8008b7ca05657cd8f5360f877c8aa85
Ezequiel <ezequielurrizaga@smartadtags.com>
2017-12-26 11:30:13 -0300

    fin proyecto 4

--- proyecto 4/src/actions/index.js
+++ proyecto 4/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=ezeu123987';
 
@@ -27,10 +28,20 @@ export function createPost(values, callback) {
 }
 
 export function fetchPost(id, callback) {
-    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`, values)
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
         //.then(() => callback());
     return {
         type: FETCH_POST,
         payload: request
     }
+}
+
+export function deletePost(id, callback){
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+    return {
+        type: DELETE_POST,
+        payload: id
+    }
+
 }
--- proyecto 4/src/reducers/reducer_posts.js
+++ proyecto 4/src/reducers/reducer_posts.js
@@ -1,12 +1,22 @@
-import { FETCH_POSTS } from '../actions'; //no se especifica porque es index.js
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions'; //no se especifica porque es index.js
 import _ from 'lodash';
 export default function(state = {}, action){
     switch (action.type) {
         case FETCH_POSTS: 
             return _.mapKeys(action.payload.data, 'id');
         break;
-        case FETCH_POST: 
-            return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            //ES 5
+            // const post = action.payload.data; 
+            // const newState = { ... state };
+            // newState[post.id] = post;
+            // return newState;
+            //ES 6
+            return { ...state, [action.payload.data.id]: action.payload.data }
+        break;
+        case DELETE_POST: // actualizar el state para quitar el elemento que eliminamos
+            //omit: toma un arreglo y le quita el elemento que tiene el inidice que es el segundo parámetro
+            return _.omit(state, action.payload);
         break;
         default: return state;
     }
