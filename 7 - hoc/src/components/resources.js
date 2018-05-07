bece475e9c9a909e03a26908ed23cf6fda698034
eurrizaga <ezequelu@yahoo.com.ar>
2018-05-07 00:08:12 -0300

    avances

---
+++ 7 - hoc/src/components/resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+    return (<div>
+        Receta especial
+        <ul>
+            <li>1 aaa</li>
+            <li>2 bbbb</li>
+            <li>3 cccc</li>
+        </ul>
+    </div>);
+}
